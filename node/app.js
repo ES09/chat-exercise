7fdf87f769c3f36e102f3233cc60eeb52652bd61
ES09 <e.song925@gmail.com>
2022-01-13 21:27:00 +0900

    사용자 설정

--- node/app.js
+++ node/app.js
@@ -14,15 +14,30 @@ const server = app.listen( port, () => {
 
 const io = listen(server);
 
+const color = [
+    "yellow",
+    "green",
+    "red",
+    "blue",
+    "white",
+    "black",
+]
+
 io.on('connection', (socket) => {
+    //사용자 랜덤 배정
     const username = color[ Math.floor(Math.random() * 6) ];
-    socket.broadcast.emit( 'join',  {  username  } );
+    socket.broadcast.emit( 'join',  { username } );
 
     socket.on('client message', (data) => {
         // 연결 된 모든 사람에게 표출 io.emit
         // 자기 자신 빼고 표출 socket.broadcast
-        socket.broadcast.emit('server message', {
+        io.emit('server message', {
+            username, 
             message : data.message
         });
+    });
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('leave', { username });
     });
 });
